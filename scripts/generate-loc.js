d2847ef0f082041b62defdc0465b845b538475a5
Resi Respati <resir014@gmail.com>
2023-05-10 15:40:26 +0700

    chore: npm script to generate loc files (#2)

---
+++ scripts/generate-loc.js
@@ -0,0 +1,35 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import ora from 'ora';
+import path from 'path';
+import { getFiles, toSecond } from './utils.js';
+
+const BASE_URL = 'https://trackmania-asia-oce.github.io/signpacks';
+
+async function main() {
+  const start = process.hrtime();
+  const spinner = ora(`${chalk.yellowBright('Generating new .loc files...')}`).start();
+
+  getFiles(path.resolve(process.cwd(), 'public')).then(files => {
+    const parsedFiles = files.map(path.parse).filter(file => file.ext !== '.loc');
+
+    parsedFiles.forEach(file => {
+      const relativePath = path
+        .relative(process.cwd(), path.resolve(file.dir, file.base))
+        .split('\\')
+        .join('/');
+
+      fs.writeFileSync(
+        path.resolve(file.dir, `${file.name}.loc`),
+        relativePath.replace('public/', `${BASE_URL}/`)
+      );
+    });
+
+    const end = `${toSecond(process.hrtime(start))} seconds`;
+    spinner.succeed(`Done in ${chalk.greenBright(end)}`);
+  });
+}
+
+main().catch(err => {
+  console.error(err);
+});
